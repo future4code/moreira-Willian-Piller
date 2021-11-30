779927980542b6d6622218048c336e11b47bf2df
WillianPiller <willianpiller09@gmail.com>
2021-11-30 19:03:26 -0400

    terminei as atividades

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,25 @@
+let idade = prompt("qual sua idade");
+let idade2 =prompt("qual idade do seu do seu amigo")
+let numero1 = Number(idade)
+let numero2 = Number(idade2)
+booleano = numero1>numero2;
+console.log("sua idade e maior que do seu melhor amigo?",booleano);
+let par = prompt("escreva numeros pares")
+let numeroUm = Number(par)
+resultado = numeroUm % 2;
+console.log(resultado)
+//todo numero divido por par tem resto zero
+//que os numeros impar vai resto um
+let user=prompt('qual sua idade');
+let num = Number(user)
+contaA = num * 12;
+contaB = num * 365;
+contaC = num * (24*365);
+console.log(contaA,contaB,contaC);
+let atv4_numero1 = prompt("digite numero 1")
+let atv4_numero2 = prompt("digite numero 2")
+
+console.log('O primeiro numero é maior que segundo?',atv4_numero1>atv4_numero2)
+console.log("O primeiro numero é igual ao segundo?",atv4_numero1==atv4_numero2)
+console.log("primeiro numero é divisível pelo segundo",atv4_numero1%atv4_numero2==0)
+console.log("segundo numero é divisível pelo primeiro",atv4_numero2%atv4_numero1==0)
