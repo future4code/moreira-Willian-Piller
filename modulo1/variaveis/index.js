049eadd0afbb96ff144caee070c4254e252c1500
WillianPiller <willianpiller09@gmail.com>
2021-11-30 14:01:54 -0400

    fiz a atividade de variaveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,3 +1,27 @@
 let nome = prompt("qual e seu nome ");
 let idade = prompt("qual sua idade");
 console.log(typeof nome, typeof idade);
+//porque prompt sempre vai retorna uma string
+let name = prompt("qual e seu nome ");
+let age = prompt("qual sua idade");
+console.log("seus dados:",name, age );
+//que sem typeof imprimir os dados do usuario
+let nome1 = prompt("qual e seu nome ");
+let idade1 = prompt("qual sua idade");
+console.log("ola "+ nome1, "voce tem "+ idade1 +
+" anos" );
+
+let roupas =prompt("voce esta usando roupa azul?")
+let futsal = prompt("voce joga bola?")
+let game = prompt("voce tem ps5?")
+//vou fazer 3 console.log 1 de cada pergunta e a dica ta atividade
+console.log("voce esta usando roupa azul?",roupas)
+console.log("voce joga bola?", futsal)
+console.log("voce tem ps5?",game)
+let a = 10
+let b = 25
+ a = 25
+ b = 10
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
