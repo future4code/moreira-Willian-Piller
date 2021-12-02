a187a9daf5c368d322199a3cf8a575d190585d0b
WillianPiller <willianpiller09@gmail.com>
2021-12-02 16:27:38 -0400

    atividades de funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,10 @@
+// eu sou caio tenho 18 anos, moro mato grosso e sou estudante
+function informacoes() {
+    const meuNome = "will"
+    const minhaIdade = 13
+    const minhaCidade = "colider" 
+    const souEstudante = "sim"
+    const frase = "eu sou " + meuNome + " tenho " + minhaIdade + " anos, moro " + minhaCidade  + " estudante " + souEstudante
+    return frase
+} 
+console.log(informacoes())
